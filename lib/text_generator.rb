e2cabcdbc8bc804eb8f0640147227ed3329d418e
Albert Davidson Chou <HotFusionMan@Yahoo.com>
2011-07-26 10:35:19 -0700

    Passing two examples.

--- lib/text_generator.rb
+++ lib/text_generator.rb
@@ -1,5 +1,19 @@
+require 'markov_chain'
+
 class TextGenerator
+  attr_reader :markov_chain
+
+  def initialize
+    @markov_chain = MarkovChain.new
+  end
+
   def sentences( text )
     text.scan( /[^\.!]+[\.!]/ ).each { |s| s.lstrip! }
+  end
+
+  def seed( text )
+    sentences( text ).each { |sentence|
+      @markov_chain.increment_probability( *( sentence.split ) )
+    }
   end
 end
