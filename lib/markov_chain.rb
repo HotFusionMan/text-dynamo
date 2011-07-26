3ee7a925c154c82a997c891056582d4aa8adbacb
Albert Davidson Chou <HotFusionMan@Yahoo.com>
2011-07-26 08:51:27 -0700

    Passing first two examples.

--- lib/markov_chain.rb
+++ lib/markov_chain.rb
@@ -6,16 +6,30 @@ class MarkovChain
   end
 
   def increment_probability( start_node, end_node )
+    @graph.add_node( start_node )
+    @graph.add_node( end_node )
+    @graph.connect( start_node, end_node, 1 )
   end
 end
 
 
 class Graph
+  def initialize
+    @edge_weight = Hash.new( 0 )
+  end
+
   def contains?( s )
     TRUE
   end
 
   def edge_weight( start_node, end_node )
-    1
+    @edge_weight[start_node + end_node]
+  end
+
+  def add_node( node )
+  end
+
+  def connect( start_node, end_node, weight )
+    @edge_weight[start_node + end_node] += weight
   end
 end
